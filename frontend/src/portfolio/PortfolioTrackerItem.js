2b77433238d92bf04d3c48baabc3ad1e9ad9f4ee
Maxim Geerinck <c4d3r@hotmail.com>
2017-08-22 10:11:45 +0200

    weak/soft trend indicator

--- frontend/src/portfolio/PortfolioTrackerItem.js
+++ frontend/src/portfolio/PortfolioTrackerItem.js
@@ -4,93 +4,174 @@ import cx from "classnames";
 import Loader from "../components/Loader";
 import { getCoinImage } from "../helpers/CoinHelper";
 import styles from "./portfolioTracker.scss";
-import { FaCaretUp, FaCaretDown, FaMinus } from "react-icons/lib/fa";
+import { FaCaretUp, FaCaretDown } from "react-icons/lib/fa";
 
 import { round, gained } from "../helpers/MathHelper";
 
 class PortfolioTrackerItem extends Component {
-    render() {
-        const {
-            name,
-            symbol,
-            changeHour,
-            changeDay,
-            changeWeek,
-            price,
-            amount,
-            currency,
-            id,
-            history,
-            boughtPrice,
-            rate,
-            settings
-        } = this.props;
+    static childContextTypes = {
+        reactIconBase: PropTypes.object
+    };
 
-        const loader = this.props.isUpdating ? <Loader className={styles.loader} color="#848484" /> : null;
+    getChildContext() {
+        return {
+            reactIconBase: {
+                size: "100%"
+            }
+        };
+    }
 
+    renderIndicator() {
+        const { history, price } = this.props;
+
+        let indicator = 0;
+        let priceChangeIndicator = null;
+
+        indicator = history[history.length - 1].price < price ? 1 : -1;
+
+        for (let i = history.length - 2; i >= 0; i--) {
+            if (price < history[i].price && history[i].price !== history[i + 1].price) {
+                if (indicator < 0) {
+                    indicator = 0;
+                }
+                indicator++;
+            } else if (price > history[i].price && history[i].price !== history[i + 1].price) {
+                if (indicator > 0) {
+                    indicator = 0;
+                }
+                indicator--;
+            }
+        }
+
+        switch (true) {
+            case indicator >= 3:
+                priceChangeIndicator = (
+                    <span className={cx(styles.caret, styles.positive)}>
+                        <FaCaretUp />
+                        <FaCaretUp />
+                        <FaCaretUp />
+                    </span>
+                );
+                break;
+            case indicator === 2:
+                priceChangeIndicator = (
+                    <span className={cx(styles.caret, styles.positive)}>
+                        <FaCaretUp />
+                        <FaCaretUp />
+                    </span>
+                );
+                break;
+            case indicator === 1:
+                priceChangeIndicator = (
+                    <span className={cx(styles.caret, styles.positive)}>
+                        <FaCaretUp />
+                    </span>
+                );
+                break;
+            case indicator === -1:
+                priceChangeIndicator = (
+                    <span className={cx(styles.caret, styles.negative)}>
+                        <FaCaretDown />
+                    </span>
+                );
+                break;
+            case indicator === -2:
+                priceChangeIndicator = (
+                    <span className={cx(styles.caret, styles.negative)}>
+                        <FaCaretDown />
+                        <FaCaretDown />
+                    </span>
+                );
+                break;
+            case indicator <= -3:
+                priceChangeIndicator = (
+                    <span className={cx(styles.caret, styles.negative)}>
+                        <FaCaretDown />
+                        <FaCaretDown />
+                        <FaCaretDown />
+                    </span>
+                );
+                break;
+        }
+
+        return priceChangeIndicator;
+    }
+
+    renderChange() {
+        const { changeHour, changeDay, changeWeek } = this.props;
         const classChangeHour = changeHour >= 0 ? styles.positive : styles.negative;
         const classChangeDay = changeDay >= 0 ? styles.positive : styles.negative;
         const classChangeWeek = changeWeek >= 0 ? styles.positive : styles.negative;
 
-        const calculations = `${currency} ${round(price, 6)} * ${amount} = ${currency} ${round(price * amount, 6)}`;
+        return (
+            <ul className={styles.change}>
+                <li>
+                    <span className={styles.changeType}>H</span>
+                    <span className={classChangeHour}>{changeHour}%</span>
+                </li>
+                <li>
+                    <span className={styles.changeType}>D</span>
+                    <span className={classChangeDay}>{changeDay}%</span>
+                </li>
+                <li>
+                    <span className={styles.changeType}>W</span>
+                    <span className={classChangeWeek}>{changeWeek}%</span>
+                </li>
+            </ul>
+        );
+    }
+
+    renderChangeTotal() {
+        const { boughtPrice, rate, amount, price, changeDay, settings } = this.props;
+        const changeTotal = boughtPrice ? gained(boughtPrice, price * rate) : changeDay;
+        const classChangeTotal =
+            changeTotal >= 0 ? cx(styles.changeTotal, styles.positive) : cx(styles.changeTotal, styles.negative);
+
+        const coinProfit = round(price * amount * rate - boughtPrice * amount, 6);
+
+        const coinProfitClass = coinProfit > 0 ? cx(styles.value, styles.positive) : cx(styles.value, styles.negative);
+        const coinProfitDisplay = settings.price ? <div className={coinProfitClass}>{coinProfit}</div> : null;
+
+        const priceChangeIndicator = this.renderIndicator();
+
+        return (
+            <div className={classChangeTotal}>
+                <div className={styles.percentage}>
+                    {round(changeTotal, 2)}%
+                    {priceChangeIndicator}
+                </div>
+                {coinProfitDisplay}
+            </div>
+        );
+    }
+
+    renderCalculations() {
+        const { currency, price, rate, amount } = this.props;
+        const calculations = `${currency} ${round(price * rate, 6)} * ${amount} = ${currency} ${round(
+            price * amount * rate,
+            6
+        )}`;
+        return (
+            <div className={styles.price}>
+                <span className={styles.calculations}>{calculations}</span>
+            </div>
+        );
+    }
+
+    render() {
+        const { name, symbol, id, settings } = this.props;
+
+        const loader = this.props.isUpdating ? <Loader className={styles.loader} color="#848484" /> : null;
 
         let prices, changeTotalContainer, statistics;
 
         if (settings.amount && settings.price) {
-            prices = (
-                <div className={styles.price}>
-                    <span className={styles.calculations}>{calculations}</span>
-                </div>
-            );
+            prices = this.renderCalculations();
         }
 
         if (settings.change) {
-            statistics = (
-                <ul className={styles.change}>
-                    <li>
-                        <span className={styles.changeType}>H</span>
-                        <span className={classChangeHour}>{changeHour}%</span>
-                    </li>
-                    <li>
-                        <span className={styles.changeType}>D</span>
-                        <span className={classChangeDay}>{changeDay}%</span>
-                    </li>
-                    <li>
-                        <span className={styles.changeType}>W</span>
-                        <span className={classChangeWeek}>{changeWeek}%</span>
-                    </li>
-                </ul>
-            );
-        }
-
-        if (settings.change) {
-            let priceChangeIndicator = null;
-            for (let i = history.length - 1; i >= 0; i--) {
-                if (price < history[i].price) {
-                    priceChangeIndicator = <FaCaretDown className={styles.negative} />;
-                } else if (price > history[i].price) {
-                    priceChangeIndicator = <FaCaretUp className={styles.positive} />;
-                }
-            }
-
-            const changeTotal = boughtPrice ? gained(boughtPrice, price * rate) : changeDay;
-            const classChangeTotal =
-                changeTotal >= 0 ? cx(styles.changeTotal, styles.positive) : cx(styles.changeTotal, styles.negative);
-
-            const coinProfit = round(price * amount * rate - boughtPrice * amount, 6);
-            const coinProfitClass =
-                coinProfit > 0 ? cx(styles.value, styles.positive) : cx(styles.value, styles.negative);
-            const coinProfitDisplay = settings.price ? <div className={coinProfitClass}>{coinProfit}</div> : null;
-
-            changeTotalContainer = (
-                <div className={classChangeTotal}>
-                    <div className={styles.percentage}>
-                        {round(changeTotal, 2)}%
-                        <span className={styles.caret}>{priceChangeIndicator}</span>
-                    </div>
-                    {coinProfitDisplay}
-                </div>
-            );
+            statistics = this.renderChange();
+            changeTotalContainer = this.renderChangeTotal();
         }
 
         return (
