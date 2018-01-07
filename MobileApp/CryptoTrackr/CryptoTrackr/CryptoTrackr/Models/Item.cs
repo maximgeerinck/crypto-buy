adb4e00af2a6456f3dac6a2cae4ff099eeb45ae1
Xavier Geerinck <thebillkidy@gmail.com>
2018-01-07 21:16:34 +0100

    Add MobileApp

---
+++ MobileApp/CryptoTrackr/CryptoTrackr/CryptoTrackr/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using Microcharts;
+using System;
+
+namespace CryptoTrackr
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
