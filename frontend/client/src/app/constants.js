7448df1bcc5207845848aece71ec33b11013de51
Maxim Geerinck <c4d3r@hotmail.com>
2018-01-08 16:52:15 +0100

    Change version fix build

--- frontend/client/src/app/constants.js
+++ frontend/client/src/app/constants.js
@@ -1,5 +1,5 @@
 /* global process */
 export const BASE_PATH = process.env.SERVICE_URL;
-export const APP_VERSION = 9;
+export const APP_VERSION = 10;
 export const TRACKING_ID = "UA-101986081-1";
 export const TITLE = "Crypto Portfolio tracker";
